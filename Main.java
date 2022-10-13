4806335407b74bb8c3035d1e39fbd6ea8cb36a2a
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-10-13 17:00:40 +0530

    Problem 2 | Approach 1

--- Main.java
+++ Main.java
@@ -1,28 +1,43 @@
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
        /*
        Bit manipulation:
-       1. find the only non-repeating element in an array
+       2. find the two non-repeating element in an array
        where every element repeats twice
         */
 
-        // approach 2:
-        int a[] = {5,4,1,4,3,5,1} ;
-        int result = 0 ;
+        // approach 1:
+        int a[] = {2, 4, 7, 9, 2, 4} ;
 
-       for(int i = 0 ; i < a.length; i++){
-           result = result ^ a[i] ;
-       }
+        int result = 0 ;
+        for(int i=0; i<a.length ; i++){
+            result = result ^ a[i] ;
+        }
+        // find right most set bit
+        result = (result & -result) ;
         System.out.println(result);
+
+        int sum1 = 0 ;
+        int sum2 = 0 ;
+        for (int i = 0; i< a.length; i++){
+            if((a[i]&result)>0){
+                sum1 = sum1 ^ a[i] ;
+            }else {
+                sum2 = sum2 ^ a[i] ;
+            }
+        }
+        System.out.println(sum1 + " " + sum2);
 
     }
 }
 
 /*
-OUTPUT: 3
+OUTPUT: [7,9]
 TN: O(n)
 SC: O(1)
  */

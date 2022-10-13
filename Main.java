911759036dfcc7089f15b5efe82647da22aafb50
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-10-13 15:14:42 +0530

    Bit Manipulation Techniques | Problem 1 | Approach 1

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+       /*
+       Bit manipulation:
+       1. find the only non-repeating element in an array
+       where every element repeats twice
+        */
+
+        // approach 1:
+        int a[] = {5,4,1,4,3,5,1} ;
+
+        Set<Integer> s = new HashSet<>() ;
+        for(int i = 0; i< a.length; i++){
+            if(!s.contains(a[i])){
+                s.add(a[i]) ;
+            }else {
+                s.remove(a[i]) ;
+            }
+        }
+        System.out.println(s);
     }
 }
+
+/*
+OUTPUT: 3
+TN: O(n)
+SC: O(n)
+ */

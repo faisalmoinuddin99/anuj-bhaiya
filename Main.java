ad5df37cc29588e3ec1cfe6fe40af23729cbe4e1
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-10-16 01:17:36 +0530

    Sieve of Eratosthenes Algorithm

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -5,30 +6,36 @@ import static java.util.Arrays.stream;
 
 public class Main {
     public static void main(String[] args) {
-    // Mathmatics algorithm
+        // Mathematics algorithm
 
         /*
-        Palindrom Number
+            sieve of eratosthenes: Is used to find prime no from 2 to n
          */
+        int n = 50 ;
+        boolean result [] = sieveOfEratosthenes(n) ;
+        for(int i = 0; i < n ;i++){
+           if(result[i]){
+               System.out.print(i + " ");
+           }
+        }
 
-      int n = 125 ;
-      int lastDigit = 0;
-      int reverse = 0 ;
-        int temp = n ;
-      while(n != 0){
-          lastDigit = n % 10 ;
-          reverse = reverse * 10 + lastDigit ;
-          n = n / 10 ;
-      }
-      if(temp == reverse){
-          System.out.println("Palindrom");
-      }else {
-          System.out.println("Not a Palindrom");
-      }
+    }
+    public static boolean[] sieveOfEratosthenes(int n){
+        boolean isPrime[] = new boolean[n+1] ;
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false ;
+        isPrime[1] = false ;
+        for(int i = 2; i*i <= n ; i++){
 
+            for(int j = 2 * i; j <= n ; j += i){
+//                System.out.print(j + " ");
+                isPrime[j] = false ;
+            }
+        }
+        return isPrime ;
     }
 }
 
 /*
-OUTPUT: 24
+OUTPUT: Prime no from 2 to 50 : 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
  */

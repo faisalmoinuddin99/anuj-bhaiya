502ed068abe3ca46df151612f6feb16e72bf08b7
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-10-15 06:40:07 +0530

    Single Number 2 | Set Bit Approach

--- Main.java
+++ Main.java
@@ -1,26 +1,28 @@
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.HashSet;
+import java.util.Set;
 
 import static java.util.Arrays.stream;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        Sort Integers by The Number of 1 Bits
-         */
-        int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 
-        Integer[] wrapperArray = Arrays.stream(a).boxed().toArray(Integer[]::new);
-        Arrays.sort(wrapperArray, (x, b) -> Integer.bitCount(x) == Integer.bitCount(b) ? x - b :
-                Integer.bitCount(x) - Integer.bitCount(b)
-        );
+        int a[] = {4,4,4,2,2,2,1,3,3,3};
 
-        int p[] = Arrays.stream(wrapperArray).mapToInt(Integer::intValue).toArray() ;
-        for(int i = 0; i<p.length; i++){
-            System.out.println(p[i]);
+
+        int ans = 0 ;
+        for(int i = 0; i < 31; i++){
+            int sum = 0 ;
+            for(int j = 0 ; j < a.length ;j++){
+                if(((a[j] >> i) & 1 ) == 1) {
+                    // n & 1 == 0 ? even : odd
+                    sum++ ;
+                }
+                sum = sum % 3 ;
+            }
+            if(sum != 0){
+                ans |= sum << i ; // ye bit set kr ne k liye hai
+            }
         }
-
-
+        System.out.println(ans);
     }
 }

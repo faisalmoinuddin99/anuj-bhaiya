7402c1aec2b0d1d7fc484060c8a07875859aad04
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-10-15 12:29:01 +0530

    MathAlgorithm | Factorial

--- Main.java
+++ Main.java
@@ -5,24 +5,18 @@ import static java.util.Arrays.stream;
 
 public class Main {
     public static void main(String[] args) {
+    // Mathmatics algorithm
 
-        int a[] = {4,4,4,2,2,2,1,3,3,3};
+        /*
+        Factorial
+         */
 
-
-        int ans = 0 ;
-        for(int i = 0; i < 31; i++){
-            int sum = 0 ;
-            for(int j = 0 ; j < a.length ;j++){
-                if(((a[j] >> i) & 1 ) == 1) {
-                    // n & 1 == 0 ? even : odd
-                    sum++ ;
-                }
-                sum = sum % 3 ;
-            }
-            if(sum != 0){
-                ans |= sum << i ; // ye bit set kr ne k liye hai
-            }
+        int n = 5 ;
+        int fact = 1 ;
+        while(n != 0){
+            fact = fact * n ;
+            n = n - 1 ;
         }
-        System.out.println(ans);
+        System.out.println(fact);
     }
 }

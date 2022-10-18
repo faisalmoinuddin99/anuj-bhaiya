4a8be9cbf15fa3b96da176defab952b53c1ade61
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-10-18 18:09:07 +0530

    How can we invert the alphabet’s case?

--- Main.java
+++ Main.java
@@ -12,27 +12,39 @@ public class Main {
             Bit Hacks — Part 4 (Playing with letters of the English alphabet)
          */
 
-        // Trick 1. Convert uppercase character to lowercase
-        char ch = 'n' ;
+        //  1. Convert uppercase character to lowercase
+        char ch = 'j' ;
+        System.out.println("Input from use: " + ch);
         char lowerCh = upperToLower(ch) ;
         char upperCh = lowerToUpper(ch) ;
         System.out.println("Converted from upper to lower: "+lowerCh);
         System.out.println("Converted from lower to upper: "+upperCh);
+
+        char invertAplha = invertAlphabet(ch) ;
+        System.out.println("Invert the alphabet: "+invertAplha);
     }
     public static char upperToLower(char ch){
         int ans = ch | ' ' ;
 
         return (char) ans ;
      }
-    // Trick 2. Convert lowercase character to uppercase
+    //  2. Convert lowercase character to uppercase
 
     public static char lowerToUpper(char ch){
         int ans = ch & '_' ;
 
         return (char) ans ;
     }
+    // 3. How can we invert the alphabet’s case?
+
+    public static char invertAlphabet(char ch){
+        int ans = ch ^ ' ' ;
+        return (char) ans ;
+    }
 }
 /*
-Converted from upper to lower: n
-Converted from lower to upper: N
+Input from use: j
+Converted from upper to lower: j
+Converted from lower to upper: J
+Invert the alphabet: J
  */

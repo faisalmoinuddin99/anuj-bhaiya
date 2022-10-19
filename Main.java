98c9f3bb11b5200609280be33e993fe1e1e2a8d9
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-10-19 09:29:10 +0530

    Find the total number of bits needed to be flipped

--- Main.java
+++ Main.java
@@ -9,25 +9,29 @@ public class Main {
     public static void main(String[] args) {
         /*
             Important Properties:
-          Bit Hacks – Part 5 (Find the absolute value of an integer without branching)
+         Part 6 (Find the total number of bits needed to be flipped)
          */
-        int n = -18 ;
-        int mask = Integer.SIZE * 8 - 1  ;
-        int abs = (n + mask) ^ mask ;
-        System.out.println("abs(" + n +")= " + abs);
-
-        System.out.println("Integer size: "+Integer.SIZE);
-        System.out.println("Long size: "+Long.SIZE);
-
+        int ans = minBitFlips(65,80) ;
+        System.out.println(ans);
 
     }
 
+    public static int minBitFlips(int start, int goal) {
+
+        int XOR = start ^ goal ;
+
+      // using Brain Keringhan Algorithm
+        int cnt = 0 ;
+        while (XOR != 0) {
+
+            XOR = XOR & (XOR - 1) ;
+            cnt++ ;
+        }
+        return cnt ;
+    }
 }
 
 /*
-OUTPUT:
-abs(-18)= 18
-Integer size: 32
-Long size: 64
+
 
  */

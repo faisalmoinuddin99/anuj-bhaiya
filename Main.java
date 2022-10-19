d5169303017b23ee12172b0a5bf2a8c2fc59e28d
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-10-19 09:05:57 +0530

    Bit Hacks – Part 5 (Find the absolute value of an integer without branching)

--- Main.java
+++ Main.java
@@ -9,51 +9,25 @@ public class Main {
     public static void main(String[] args) {
         /*
             Important Properties:
-            Bit Hacks — Part 4 (Playing with letters of the English alphabet)
+          Bit Hacks – Part 5 (Find the absolute value of an integer without branching)
          */
+        int n = -18 ;
+        int mask = Integer.SIZE * 8 - 1  ;
+        int abs = (n + mask) ^ mask ;
+        System.out.println("abs(" + n +")= " + abs);
 
-        //  1. Convert uppercase character to lowercase
-        char ch = 'd' ;
-        System.out.println("Input from use: " + ch);
-        char lowerCh = upperToLower(ch) ;
-        char upperCh = lowerToUpper(ch) ;
-        System.out.println("Converted from upper to lower: "+lowerCh);
-        System.out.println("Converted from lower to upper: "+upperCh);
+        System.out.println("Integer size: "+Integer.SIZE);
+        System.out.println("Long size: "+Long.SIZE);
 
-        char invertAlpha = invertAlphabet(ch) ;
-        System.out.println("Invert the alphabet: "+ invertAlpha);
 
-        int positionOfAlpha = positionOfAlphabet(ch) ;
-        System.out.println("Position: " + positionOfAlpha);
-    }
-    public static char upperToLower(char ch){
-        int ans = ch | ' ' ;
-
-        return (char) ans ;
-     }
-    //  2. Convert lowercase character to uppercase
-
-    public static char lowerToUpper(char ch){
-        int ans = ch & '_' ;
-
-        return (char) ans ;
-    }
-    // 3. How can we invert the alphabet’s case?
-
-    public static char invertAlphabet(char ch){
-        int ans = ch ^ ' ' ;
-        return (char) ans ;
     }
 
-    // 4. Find a letter’s position in alphabet
-    public static int positionOfAlphabet(char ch){
-        int position = ch & 31 ;
-        return position ;
-    }
 }
+
 /*
-Input from use: j
-Converted from upper to lower: j
-Converted from lower to upper: J
-Invert the alphabet: J
+OUTPUT:
+abs(-18)= 18
+Integer size: 32
+Long size: 64
+
  */

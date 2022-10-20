219da5bd3d164ae5a2183e404b1feb17375f61e8
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-10-20 14:51:24 +0530

    Count total set bits in all numbers from 1 to n

--- Main.java
+++ Main.java
@@ -4,34 +4,52 @@ import static java.util.Arrays.stream;
 
 public class Main {
     public static void main(String[] args) {
+/*
 
-        /*
 
-        Input: A = 10, B = 20
-        Output: 4
+        Input: N = 4
+        Output: 5
         Explanation:
-        A  = 01010
-        B  = 10100
-        As we can see, the bits of A that need
-        to be flipped are 01010. If we flip
-        these bits, we get 10100, which is B.
+        For numbers from 1 to 4.
+        For 1: 0 0 1 = 1 set bits
+        For 2: 0 1 0 = 1 set bits
+        For 3: 0 1 1 = 2 set bits
+        For 4: 1 0 0 = 1 set bits
+        Therefore, the total set bits is 5.
 
-         */
-
-        int A = 20, B = 25   ;
-        int xor = A ^ B ;
-
-        // Brains Algorithm
-        int cntDifferBits = 0 ;
-        while(xor != 0){
-            xor = xor & (xor - 1) ;
-            cntDifferBits++ ;
-        }
-
-        System.out.println(cntDifferBits);
+ */
 
 
+       int ans = countNoOfSetBits(17) ;
+        System.out.println(ans);
 
     }
-}
 
+    public static int countNoOfSetBits(int n) {
+        if(n == 0){
+            return 0;
+        }
+
+        int x = largestPowerOf2InRange(n) ;
+        int noOfOnesBeforeLargestPowerOf2 = x * (1 << (x - 1)) ;
+        int noOfOnesAfterLargestPowerOf2 = (n - (1 << x) + 1) ;
+        int rest = (n - (1<<x)) ;
+
+        int ans = noOfOnesBeforeLargestPowerOf2 + noOfOnesAfterLargestPowerOf2 + countNoOfSetBits(rest) ;
+
+        return ans ;
+
+    }
+
+    public static int largestPowerOf2InRange(int n) {
+        int x = 0 ;
+        while ((1<<x) <= n){
+            x++ ;
+        }
+        return x - 1;
+    }
+
+}
+/*
+35
+ */

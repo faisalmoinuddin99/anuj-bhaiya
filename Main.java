35011ec767b325374472125c853be369311299ee
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-10-21 08:12:56 +0530

    Find position of set bit

--- Main.java
+++ Main.java
@@ -4,52 +4,30 @@ import static java.util.Arrays.stream;
 
 public class Main {
     public static void main(String[] args) {
-/*
-
-
-        Input: N = 4
-        Output: 5
-        Explanation:
-        For numbers from 1 to 4.
-        For 1: 0 0 1 = 1 set bits
-        For 2: 0 1 0 = 1 set bits
-        For 3: 0 1 1 = 2 set bits
-        For 4: 1 0 0 = 1 set bits
-        Therefore, the total set bits is 5.
-
- */
-
-
-       int ans = countNoOfSetBits(17) ;
+        /*
+            Find position of set bit
+         */
+        int ans = findPosition(1) ;
         System.out.println(ans);
-
     }
 
-    public static int countNoOfSetBits(int n) {
-        if(n == 0){
-            return 0;
-        }
-
-        int x = largestPowerOf2InRange(n) ;
-        int noOfOnesBeforeLargestPowerOf2 = x * (1 << (x - 1)) ;
-        int noOfOnesAfterLargestPowerOf2 = (n - (1 << x) + 1) ;
-        int rest = (n - (1<<x)) ;
-
-        int ans = noOfOnesBeforeLargestPowerOf2 + noOfOnesAfterLargestPowerOf2 + countNoOfSetBits(rest) ;
-
-        return ans ;
-
+   public static int findPosition(int N) {
+        // code here
+       int cnt = 0 ;
+       if (!isPowerOf2(N)){
+           return -1 ;
+       }else{
+           while(N != 0){
+               if((N & 1) == 1){
+                   break ;
+               }
+               cnt++ ;
+               N = N >> 1 ;
+           }
+       }
+       return cnt + 1;
     }
-
-    public static int largestPowerOf2InRange(int n) {
-        int x = 0 ;
-        while ((1<<x) <= n){
-            x++ ;
-        }
-        return x - 1;
+    public static boolean isPowerOf2(int n){
+        return n > 0 && (n & (n-1)) == 0 ;
     }
-
 }
-/*
-35
- */

4eb95b8fcedc38ad914d6a37f63e57a1ccb0caf9
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-10-28 20:09:21 +0530

    Check Permutation

---
+++ Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String s1 = "god" ;
+        String s2 = "god" ;
+
+       boolean result =  isPermutated(s1,s2);
+        System.out.println(result);
+    }
+    public static boolean isPermutated(String s1, String s2) {
+        boolean[] flag = new boolean[s1.length()] ;
+
+        for (int i = 0 ; i < s1.length(); i++){
+            for(int j = 0 ; j < s2.length(); j++){
+                if(s1.charAt(i) == s2.charAt(j)){
+                    flag[i] = true ;
+                    break ;
+                }
+            }
+        }
+
+        if (!checkBooleanArray(flag)){
+            return false ;
+        }
+        return true ;
+    }
+
+    public static boolean checkBooleanArray(boolean[] a){
+       boolean tracker = true ;
+        for (int i = 0 ; i<a.length; i++){
+            if (a[i] == false){
+                tracker = false ;
+                break ;
+            }
+        }
+        return tracker ;
+    }
+}

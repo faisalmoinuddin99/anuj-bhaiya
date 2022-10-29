3170f7f325e7b8402278ee1815fb6558f3d5e135
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-10-29 13:18:13 +0530

    URLify Cracking the coding inverview Question 3

--- Main.java
+++ Main.java
@@ -4,25 +4,39 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
 
-        String s1 = "abcd" ;
-        String s2 = "dabc" ;
-
-        boolean check = isPermutatedUsingXOR(s1,s2) ;
-        System.out.println(check);
+        String s = "Mr John Smith          ";
+        replaceWhiteSpace(s.toCharArray(), s.trim().length());
 
     }
-    public static boolean isPermutatedUsingXOR(String s1, String s2){
 
-        String s3 =  s1.concat(s2) ;
-
-        int result = 0 ;
-
-        for (int i = 0 ; i < s3.length(); i++){
-            result = result ^ s3.charAt(i) ;
+    public static void replaceWhiteSpace(char[] str, int trueLength) {
+        System.out.println(trueLength);
+        // calculate whiteSpace in true length string
+        int spaceCount = 0;
+        for (int i = 0; i < trueLength; i++) {
+            if (str[i] == ' ') {
+                spaceCount++;
+            }
         }
-        if (result != 0){
-            return false ;
+        int index;
+        index = trueLength + spaceCount * 2;
+        if (trueLength < str.length) {
+            str[trueLength] = '\0';
         }
-        return true ;
+        for (int i = trueLength - 1; i >= 0; i--) {
+            if (str[i] == ' ') {
+                str[index - 1] = '0';
+                str[index - 2] = '2';
+                str[index - 3] = '%';
+                index = index - 3;
+            } else {
+                str[index - 1] = str[i];
+                index--;
+            }
+        }
+        System.out.println(str);
     }
 }
+/*
+Mr%20John%20Smith
+ */

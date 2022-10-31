ba9f977a435c8a9b8712ffc0cfde150643f9d177
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-10-31 15:53:40 +0530

    Sort Colors: Approach 2 using Dutch National Flag Algorithm

--- Main.java
+++ Main.java
@@ -4,18 +4,42 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        int a[] = {0,1,1,0,2,1,2,0,0,0,1} ;
+        int a[] = {0, 1, 1, 0, 2, 1, 2, 0, 0, 0, 1};
+        dutchNationalFlag(a);
+    }
+    public static void dutchNationalFlag(int[] a){
 
-        Arrays.sort(a);
-        for (int i : a){
+        int low = 0 ;
+        int mid = 0 ;
+        int high = a.length - 1 ;
+        int temp ;
+        while (mid <= high){
+            switch (a[mid]){
+                case 0: {
+                  a[low] = a[low] ^ a[mid] ;
+                  a[mid] = a[low] ^ a[mid] ;
+                  a[low] = a[low] ^ a[mid] ;
+                  low++ ;
+                  mid++ ;
+                  break ;
+                }
+                case 1: {
+                    mid++ ;
+                    break;
+                }
+                case 2: {
+                    a[mid] = a[mid] ^ a[high] ;
+                    a[high] = a[mid] ^ a[high] ;
+                    a[mid] = a[mid] ^ a[high] ;
+                    high-- ;
+                    break ;
+                }
+            }
+        }
+        for (int i: a){
             System.out.print(i + " ");
         }
-    }
-}
-/*
-Approach 1: Sorting method
-TC: O(nlogn)
-SC: O(1)
 
-0 0 0 0 0 1 1 1 1 2 2
- */
+    }
+
+}

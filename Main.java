adef28f51b1d3c71af51ef3f1ca729e7a3601b5c
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-10-31 15:06:56 +0530

    Slow and Fast Approach

--- Main.java
+++ Main.java
@@ -1,47 +1,28 @@
-import java.util.HashMap;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) {
-        String s = "US POLO DESIGN UK                 ";
-        spaceManipulation(s.toCharArray(),s.trim().length());
+       int a[] = {2,3,1,2,3} ;
+       int res = findDuplicate(a) ;
+        System.out.println(res);
     }
-    public static void spaceManipulation(char[] s, int trueLength){
-        System.out.println("trueLength " + trueLength);
-      // step 1: calculate white space
-        int countSpace = 0 ;
-        for (int i = 0 ; i < trueLength; i++){
-            if (s[i] == ' '){
-                countSpace++ ;
-            }
-        }
-        // pattern = FFFF (n) = 4 = 4 - 1
-        int index = trueLength + countSpace * 3 ;
-        System.out.println("total index: " + index);
-        System.out.println("character array length: " + s.length);
-        if (trueLength < s.length){
-            s[trueLength] = '\0' ;
-        }
-        // iterating string in reverse order and find the white space and add our pattern
-        for (int i = trueLength - 1; i >= 0; i--){
-            if (s[i] == ' '){
-                s[index - 1] = 'F' ;
-                s[index - 2] = 'F' ;
-                s[index - 3] = 'F' ;
-                s[index - 4] = 'F' ;
-                index = index - 4 ;
-            }else{
-                s[index - 1] = s[i] ;
-                index-- ;
-            }
-        }
-        System.out.println(s);
-    }
-}
+    public static int findDuplicate(int [] a){
+      // using tortoise and hare algorithm
+        int tortoise = a[0] ;
+        int hare = a[0] ;
 
-/*
-trueLength 17
-total index: 26
-character array length: 34
-USFFFFPOLOFFFFDESIGNFFFFUK
- */
+        do{
+            tortoise = a[tortoise] ;
+            hare = a[a[hare]] ;
+        }while (tortoise != hare) ;
+
+        hare = a[0] ;
+        while (tortoise != hare){
+            tortoise = a[tortoise] ;
+            hare = a[hare] ;
+        }
+        return hare ;
+    }
+
+}

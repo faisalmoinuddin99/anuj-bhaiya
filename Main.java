d00cb4f1435dfe3bd8686dc80755e460b8c0f2ad
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-02 19:20:12 +0530

    Maximum Subarray sum: Kadanes Algorithm

--- Main.java
+++ Main.java
@@ -2,44 +2,41 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        int a[] = {-2,-3,4,-1,-2,1,5,-3} ;
+//        int ans = approach1(a) ;
+//        System.out.println(ans);
 
-        /*
-        [-1,0,0,3,3,3,0,0,0]
-6
-[1,2,2]
-3
-         */
-        int a[] = {-1,0,0,3,3,3,0,0,0};
-        int b[] = {1,2,2};
-       merge (a,a.length,b, b.length);
+        int res = kadanesAlgorithm(a) ;
+        System.out.println(res);
+
+    }
+    public static int approach1(int[] a){
+        int n = a.length ;
+        int max = Integer.MIN_VALUE  ;
+        for (int i = 0 ; i < n; i++){
+         int sum = 0 ;
+          for (int j = i ; j < n; j++){
+              sum += a[j] ;
+              if (sum > max){
+                  max = sum ;
+              }
+          }
+        }
+        return max ;
     }
 
-    public  static  void merge(int[] a, int m, int[] b, int n) {
-        int temp = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (a[i] == 0 || b[j] == 0) {
-                    continue;
-                } else {
-                    if (a[i] > b[j]) {
-                        temp = a[i];
-                        a[i] = b[j];
-                        b[j] = temp;
-                    }
-                }
-            }
+    public static int kadanesAlgorithm(int[] a){
+        int sum = 0 ;
+        int max = a[0] ;
+        for (int i = 0 ; i < a.length; i++){
+            sum += a[i] ;
+            // check sum is greater than max, if yes then update max
+            if (sum > max) max = sum ;
+
+            // now check sum is lesser than 0, if yes then update sum to 0
+            if (sum < 0) sum = 0 ;
 
         }
-        int index = 0;
-        for (int i = 0; i < a.length ; i++){
-            if (a[i] == 0 && index != b.length) {
-                a[i] = b[index];
-                index++;
-            }
-        }
-        for (int i : a){
-            System.out.println(i);
-        }
-
+        return max ;
     }
 }

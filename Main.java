09aa1ede6102221172462025d76a892a8f5a772b
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-03 15:05:29 +0530

    Merge Interval Approach1

--- Main.java
+++ Main.java
@@ -1,42 +1,49 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        int a[] = {-2,-3,4,-1,-2,1,5,-3} ;
-//        int ans = approach1(a) ;
-//        System.out.println(ans);
+        int[] interval1 = {1, 3};
+        int[] interval2 = {2,6};
+        int[] interval3 = {8,10};
+        int[] interval4 = {15,18};
+//        int[] interval5 = {2, 6};
+//[1,3],[2,6],[8,10],[15,18]
+        int[][] intervals = {interval1, interval2, interval3, interval4};
 
-        int res = kadanesAlgorithm(a) ;
-        System.out.println(res);
-
-    }
-    public static int approach1(int[] a){
-        int n = a.length ;
-        int max = Integer.MIN_VALUE  ;
-        for (int i = 0 ; i < n; i++){
-         int sum = 0 ;
-          for (int j = i ; j < n; j++){
-              sum += a[j] ;
-              if (sum > max){
-                  max = sum ;
-              }
-          }
+        int [][] ans = merge(intervals) ;
+        for (int []i :ans){
+            for (int j : i){
+                System.out.print(j);
+            }
         }
-        return max ;
     }
 
-    public static int kadanesAlgorithm(int[] a){
-        int sum = 0 ;
-        int max = a[0] ;
-        for (int i = 0 ; i < a.length; i++){
-            sum += a[i] ;
-            // check sum is greater than max, if yes then update max
-            if (sum > max) max = sum ;
+    public static int[][] merge(int[][] intervals) {
 
-            // now check sum is lesser than 0, if yes then update sum to 0
-            if (sum < 0) sum = 0 ;
 
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+
+        ArrayList<int[]> list = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            if (list.isEmpty()) {
+                list.add(interval);
+            } else {
+                int prevInterval[] = list.get(list.size() - 1);
+                if (interval[0] < prevInterval[1]) {
+                    // overlapping
+                    prevInterval[1] = Math.max(prevInterval[1], interval[1]);
+                } else {
+                    list.add(interval);
+                }
+            }
         }
-        return max ;
+        return list.toArray(new int[list.size()][]) ;
     }
 }
+
+// [[1,6][8,10],[15,18]]

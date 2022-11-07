6f3be20a839bfb74889b9b933d4ae902969edf6c
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-07 14:41:16 +0530

    Pascal Triangle using List Of List

--- Main.java
+++ Main.java
@@ -4,29 +4,59 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
       // pascal triangle question 1
-       trianglePattern(5);
+       trianglePattern(1);
 
 
         // question 2
         int pascalValue = findPascalValue(5,3) ;
-        System.out.println(pascalValue);
+//        System.out.println(pascalValue);
 
         // question 3
         /*
         Given nth term print all the possible value of pascal
          */
         printAllPossiblePascalValue(5);
-    }
 
+     List<List<Integer>> sol =   leetCodeSolution(1) ;
+     iterateUsingForEach(sol);
+
+
+    }
+    // Iterate the 2D list using loop
+    // and print each element
+    public static <K> void
+    iterateUsingForEach(List<List<K> > listOfLists)
+    {
+
+        // Iterate the 2D list
+        // and print each element
+        System.out.println("[");
+
+        for (List<K> list : listOfLists) {
+            System.out.print(" [");
+
+            for (K item : list) {
+                System.out.print(" "
+                        + item
+                        + ", ");
+            }
+            System.out.println("], ");
+        }
+        System.out.println("]");
+    }
     public static void trianglePattern(int line){
+
+
         int i, j, r;
         boolean k = true ;
 
         for (i = 1; i <= line; i++){
             r = 0 ;
-            for (j = 1; j <= 2 * line -1; j++){
+
+            for (j = 1; j <= 2 * line - 1; j++){
+
                 if (j>=line+1-i && j <= line-1+i && k){
-                    System.out.printf("%2d",combination(i - 1, r));
+//                    System.out.printf("%2d",combination(i - 1, r));
                     r++ ;
                     k = false ;
                 }else{
@@ -34,8 +64,10 @@ public class Main {
                     k = true ;
                 }
             }
-            System.out.println();
+//            System.out.println();
         }
+
+
 
     }
     public static int factorial(int n){
@@ -60,9 +92,32 @@ public class Main {
     public static void printAllPossiblePascalValue(int n){
         int r = 0 ;
         for (int i = 1; i <= n; i++){
-            System.out.print(combination(n-1,r) + " ");
+//            System.out.print(combination(n-1,r) + " ");
             r++ ;
         }
+    }
+
+    public static List<List<Integer>> leetCodeSolution(int numRows){
+        if (numRows == 0){
+
+            return new ArrayList<>() ;
+        }
+        List<List<Integer>> result = new ArrayList<>() ;
+        int r = 0 ;
+        for (int i = 1 ; i <= numRows; i++){
+            List<Integer> row = new ArrayList<>() ;
+            for (int j = 0; j < i ; j++){
+//                row.add(j) ;
+                if (j == 0 || j == i - 1){
+                    row.add(1) ;
+                }else{
+                    row.add(result.get(i-2).get(j) + result.get(i-2).get(j-1)) ;
+
+                }
+            }
+            result.add(row) ;
+        }
+       return result ;
     }
 }
 

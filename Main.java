df8fac97dbb60b5a5f2a950c4d84960573d5bb1e
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-08 11:33:35 +0530

    Count Inversions in an array | Approach 1: TC: O(n2)

--- Main.java
+++ Main.java
@@ -1,34 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        // Next Permutation
-        int[] a = {1, 3, 5, 4, 2} ;
+        int [] a = {8,4,2,1} ;
+        int app1 = countInversionApproach1(a) ;
+        System.out.println(app1);
+    }
 
-        nextPermutation(a);
-        for(int i: a){
-            System.out.print(i + " "); // 1 4 2 3 5
+    public static int countInversionApproach1(int [] a){
+        int cnt = 0 ;
+        for (int i = 0 ; i < a.length; i++){
+            for (int j = i ; j < a.length; j++){
+                if (a[i] > a[j]){
+                    cnt++ ;
+                }
+            }
         }
-    }
-    public static void nextPermutation(int [] a){
-        if (a == null || a.length <= 1) return;
-
-        // check the break points
-        int i = a.length - 2 ;
-
-        while (i >= 0 && a[i] >= a[i+1]) i-- ;
-
-        if (i >= 0){
-            int j = a.length - 1;
-            while (a[j] <= a[i]) j-- ;
-            swap(a,i,j);
-        }
-        reverse(a,i+1,a.length - 1);
-    }
-    public static void swap(int [] a, int i, int j){
-        int temp = a[i] ;
-        a[i] = a[j] ;
-        a[j] = temp ;
-    }
-    public static void reverse(int [] a, int i, int j){
-        while (i < j) swap(a,i++,j--);
+        return cnt ;
     }
 }

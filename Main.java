eb5f8319f9499b50bd04a9e562866d85d540173d
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-09 10:15:59 +0530

    Merge Sort By myself

--- Main.java
+++ Main.java
@@ -1,78 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        int [] a = {8,4,2,1} ;
-        int app1 = countInversionApproach1(a) ;
-        int n = a.length ;
-        System.out.println(app1);
+        int a[] = {8, 4, 2, 1};
         for (int i: a){
             System.out.print(i + " ");
         }
-        mergeSort(a,0,n - 1);
         System.out.println();
+        divide(a, 0, a.length - 1);
         for (int i: a){
             System.out.print(i + " ");
+        }
+
+    }
+
+    public static void divide(int[] A, int low, int high) {
+        if (low < high) {
+            int mid = low + (high - low) / 2;
+            divide(A, low, mid);
+            divide(A, mid + 1, high);
+            conqure(A, low, mid, high);
         }
     }
 
-    public static int countInversionApproach1(int [] a){
-        int cnt = 0 ;
-        for (int i = 0 ; i < a.length; i++){
-            for (int j = i ; j < a.length; j++){
-                if (a[i] > a[j]){
-                    cnt++ ;
-                }
+    public static void conqure(int[] A, int low, int mid, int high) {
+        int i = low;
+        int j = mid + 1;
+        int x = 0;
+        int b[] = new int[high - low + 1];
+        while ((i <= mid) && (j <= high)) {
+            if (A[i] <= A[j]) {
+                b[x++] = A[i++];
+            } else {
+                b[x++] = A[j++];
             }
         }
-        return cnt ;
-    }
-    public static void mergeSort(int [] A, int left, int right){
-        // to perform sorting array required more than one element
-        int mid = 0 ;
-        if(left >= right) return;
-            // iska matlab array mai more than one element exist
-
-            //  Mid calculate kr lete hai
-             mid = left + ( right - left) / 2 ;
-
-            // first wala, 0 index se mid index tak sort kr dega
-            mergeSort(A,left,mid);
-            // second wala, mid + 1 index se right index tak sort kr dega
-            mergeSort(A,mid+1,right);
-            // merge function, ye suppose lekr chal ta hai ki left array and right array sorted hai
-            // aur mujhe sirf merge kr na hai
-            merge(A,left,mid,right);
-
-    }
-    public static void merge(int[] A ,int left, int mid, int right){
-
-
-          int merged [] = new int[right - left + 1] ;
-
-          int idx1 = left ;
-          int idx2 = mid + 1 ;
-          int x = 0 ;
-
-          while ((idx1 <= mid) && (idx2 <= right)){
-              if (A[idx1] <= A[idx2]){
-                  merged[x++] = A[idx1++] ;
-              }else{
-                  merged[x++] = A[idx2++] ;
-              }
-          }
-        while (idx1 <= mid){
-            merged[x++] = A[idx1++] ;
+        while (i <= mid) {
+            b[x++] = A[i++];
         }
-        while (idx2 <= right){
-            merged[x++] = A[idx2++] ;
+        while (j <= high) {
+            b[x++] = A[j++];
         }
-
-        for (int i = 0, j = left; i < merged.length; i++,j++){
-            A[j] = merged[i] ;
+        for(int ii = 0, jj = low; ii < b.length; ii++,jj++){
+            A[jj] = b[ii] ;
         }
     }
 }
-
-/*
-8 4 2 1
-1 2 4 8
- */

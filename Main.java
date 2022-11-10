522f31482c88690ebbc54fc3623cc7af5095dada
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-10 16:09:57 +0530

    Rotate by 90 degree

--- Main.java
+++ Main.java
@@ -14,6 +14,7 @@ public class Main {
         int row = matrix.length;
         int col = matrix[0].length;
 
+
         for (int i = 0; i < row; i++) {
             // inner for loop with runs till diagonal of a matrix
             for (int j = 0; j <= i; j++) {
@@ -28,6 +29,25 @@ public class Main {
             System.out.print("]");
             System.out.println();
         }
+
+
+
+        for (int i = 0; i < (row / 2); i++) {
+            for (int j = 0; j < col; j++) {
+
+                enhanceSwap(matrix, i, j, row);
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < row; i++) {
+            System.out.print("[ ");
+            for (int j = 0; j < col; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.print("]");
+            System.out.println();
+        }
+
     }
 
     public static void swap(int[][] x, int i, int j) {
@@ -36,15 +56,22 @@ public class Main {
         x[i][j] = x[j][i];
         x[j][i] = temp;
     }
+
+    public static void enhanceSwap(int[][] x, int i, int j, int row) {
+        int temp = 0;
+        temp = x[i][j];
+        x[i][j] = x[row - 1 - i][j];
+        x[row - 1 - i][j] = temp;
+    }
 }
 
 /*
 INPUT:
-{1, 2, 3}
-{4, 5, 6}
-{7, 8, 9}
-OUTPUT:
 [ 1 4 7 ]
 [ 2 5 8 ]
 [ 3 6 9 ]
+OUTPUT:
+[ 3 6 9 ]
+[ 2 5 8 ]
+[ 1 4 7 ]
  */

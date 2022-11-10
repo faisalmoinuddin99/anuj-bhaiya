ffcc2239be1fb72f77648487afef5e1938fdcc9f
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-10 14:42:44 +0530

    Transpose of A matrix

--- Main.java
+++ Main.java
@@ -2,42 +2,49 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        /* snakePattern */
-        int[] a = {45,48,54};
-        int[] b = {21, 89, 87};
-        int[] c = {70, 78, 15};
+        /*
+            Transpose of a Matrix
+         */
+        int[] a = {1, 2, 3};
+        int[] b = {4, 5, 6};
+        int[] c = {7, 8, 9};
 
         int[][] matrix = {a, b, c};
 
         int row = matrix.length;
         int col = matrix[0].length;
-        int cnt = 0 ;
-       for(int i = 0 ; i < row; i++){
 
-               if (cnt != 0){
-                   System.out.print("[");
-                   for(int j = col - 1; j >= 0; j--){
-                       System.out.print(matrix[i][j] + " ");
-                       cnt-- ;
-                   }
-                   System.out.print("]");
-                   System.out.println();
-               }else{
-                   System.out.print("[");
-                   for(int j = 0; j < col; j++){
-                       System.out.print(matrix[i][j] + " ");
-                       cnt++ ;
-                   }
-                   System.out.print("]");
-                   System.out.println();
-               }
+        for (int i = 0; i < row; i++) {
+            // inner for loop with runs till diagonal of a matrix
+            for (int j = 0; j <= i; j++) {
+                swap(matrix, i, j);
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            System.out.print("[ ");
+            for (int j = 0; j < col; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.print("]");
+            System.out.println();
+        }
+    }
 
-       }
-
+    public static void swap(int[][] x, int i, int j) {
+        int temp;
+        temp = x[i][j];
+        x[i][j] = x[j][i];
+        x[j][i] = temp;
     }
 }
+
 /*
-[45 48 54 ]
-[87 89 21 ]
-[70 78 15 ]
+INPUT:
+{1, 2, 3}
+{4, 5, 6}
+{7, 8, 9}
+OUTPUT:
+[ 1 4 7 ]
+[ 2 5 8 ]
+[ 3 6 9 ]
  */

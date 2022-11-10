523180fd86046240fec6f4f04fd4273144bb4381
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-10 12:09:35 +0530

    Snake Pattern

--- Main.java
+++ Main.java
@@ -2,56 +2,42 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        /* Best Time to Buy and Sell Stock */
-        int[] a = {6, 5, 4};
-        int[] b = {1, 2, 5};
-        int[] c = {7, 9, 7};
+        /* snakePattern */
+        int[] a = {45,48,54};
+        int[] b = {21, 89, 87};
+        int[] c = {70, 78, 15};
 
         int[][] matrix = {a, b, c};
 
         int row = matrix.length;
         int col = matrix[0].length;
+        int cnt = 0 ;
+       for(int i = 0 ; i < row; i++){
 
-        // row : 3, col: 3
+               if (cnt != 0){
+                   System.out.print("[");
+                   for(int j = col - 1; j >= 0; j--){
+                       System.out.print(matrix[i][j] + " ");
+                       cnt-- ;
+                   }
+                   System.out.print("]");
+                   System.out.println();
+               }else{
+                   System.out.print("[");
+                   for(int j = 0; j < col; j++){
+                       System.out.print(matrix[i][j] + " ");
+                       cnt++ ;
+                   }
+                   System.out.print("]");
+                   System.out.println();
+               }
 
-        int upperSum = 0;
-        int lowerSum = 0;
-        for (int i = 0; i < row; i++) {
-            System.out.print("[ ");
-            for (int j = i; j < col; j++) {
-                System.out.print(matrix[i][j] + " ");
-                upperSum += matrix[i][j];
+       }
 
-            }
-            System.out.print("]");
-            System.out.println();
-        }
-        System.out.println();
-        for (int i = 0; i < row; i++) {
-            System.out.print("[ ");
-            for (int j = 0; j <= i; j++) {
-                System.out.print(matrix[i][j] + " ");
-                lowerSum += matrix[i][j];
-            }
-            System.out.print("]");
-            System.out.println();
-        }
-
-        System.out.println("UpperSum: " + upperSum);
-        System.out.println("LowerSum: " + lowerSum);
     }
-
-
 }
 /*
-[ 6 5 4 ]
-[ 2 5 ]
-[ 7 ]
-
-[ 6 ]
-[ 1 2 ]
-[ 7 9 7 ]
-UpperSum: 29
-LowerSum: 32
+[45 48 54 ]
+[87 89 21 ]
+[70 78 15 ]
  */
-

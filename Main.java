b6043eb2829bc8362fee58d29d3238f7e2626e2e
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-10 11:08:29 +0530

    GFG Sum of Upper & Lower Triangles

--- Main.java
+++ Main.java
@@ -3,30 +3,55 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         /* Best Time to Buy and Sell Stock */
-        int[] a = {7,6,4,3,1};
-        bruteForce(a);
-    }
+        int[] a = {6, 5, 4};
+        int[] b = {1, 2, 5};
+        int[] c = {7, 9, 7};
 
-    public static void bruteForce(int[] a) {
+        int[][] matrix = {a, b, c};
 
-        List<Integer> list = new ArrayList( );
-        int difference = 0;
-        int x = 0 ;
-        int maxProfit = 0 ;
-        for (int i = 0; i < a.length; i++) {
+        int row = matrix.length;
+        int col = matrix[0].length;
 
-            for (int j = i + 1; j < a.length; j++) {
-                if (a[i] < a[j]) {
-                    difference = Math.abs(a[j] - a[i]) ;
-                    list.add(difference) ;
-                }
-              if (!list.isEmpty()){
-                  maxProfit = (int) Collections.max(list) ;
-              }
+        // row : 3, col: 3
+
+        int upperSum = 0;
+        int lowerSum = 0;
+        for (int i = 0; i < row; i++) {
+            System.out.print("[ ");
+            for (int j = i; j < col; j++) {
+                System.out.print(matrix[i][j] + " ");
+                upperSum += matrix[i][j];
+
             }
-
+            System.out.print("]");
+            System.out.println();
         }
-        System.out.println(maxProfit);
+        System.out.println();
+        for (int i = 0; i < row; i++) {
+            System.out.print("[ ");
+            for (int j = 0; j <= i; j++) {
+                System.out.print(matrix[i][j] + " ");
+                lowerSum += matrix[i][j];
+            }
+            System.out.print("]");
+            System.out.println();
+        }
+
+        System.out.println("UpperSum: " + upperSum);
+        System.out.println("LowerSum: " + lowerSum);
     }
+
 
 }
+/*
+[ 6 5 4 ]
+[ 2 5 ]
+[ 7 ]
+
+[ 6 ]
+[ 1 2 ]
+[ 7 9 7 ]
+UpperSum: 29
+LowerSum: 32
+ */
+

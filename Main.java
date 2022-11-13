6b6a35e113a44f4310ad7f63b63a077cdf5f0995
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-13 10:40:28 +0530

    BinarySearch On Descending Order

--- Main.java
+++ Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
         // Binary search Algorithm
-        int a[] = {4,5,6,10,11} ;
-        int ans = binarySearch(a,5) ;
+        int a[] = {11,10,6,5,4} ;
+        int ans = binarySearchForDescendingOrder(a,5) ;
         System.out.println(ans);
     }
 
-    public static int binarySearch(int[] a, int target){
+    public static int binarySearchForAscendingOrder(int[] a, int target){
         int start = 0 ;
         int end = a.length - 1;
         int mid = 0 ;
@@ -20,6 +20,23 @@ public class Main {
                 return mid ;
             }
             if (target < a[mid]){
+                end = mid - 1 ;
+            }else {
+                start = mid + 1 ;
+            }
+        }
+        return -1 ;
+    }
+    public static int binarySearchForDescendingOrder(int[] a, int target){
+        int start = 0 ;
+        int end = a.length - 1 ;
+        int mid = 0 ;
+        while (start <= end){
+            mid = start + ((end - start) / 2);
+            if (a[mid] == target){
+                return mid ;
+            }
+            if (a[mid] < target) {
                 end = mid - 1 ;
             }else {
                 start = mid + 1 ;

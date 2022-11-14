c4c0a6d6bf06faf6019dbe9063241706d699ebb1
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-14 11:17:51 +0530

    Find First and Last Position of Element in Sorted Array

--- Main.java
+++ Main.java
@@ -2,46 +2,59 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        // Binary search Algorithm
-        int a[] = {11,10,6,5,4} ;
-        int ans = binarySearchForDescendingOrder(a,5) ;
-        System.out.println(ans);
+        int[] a = {} ;
+       int fs = bSFirstOccurance(a,0);
+       int ls = lastOccurance(a,0) ;
+       int [] result = firstAndLastOccurance(fs,ls) ;
+       for (int i : result){
+           System.out.print(i + " ");
+       }
     }
+    public static int bSFirstOccurance(int [] a, int target) {
 
-    public static int binarySearchForAscendingOrder(int[] a, int target){
-        int start = 0 ;
+        int start = 0;
         int end = a.length - 1;
-        int mid = 0 ;
-        while (start <= end){
-             mid = start + ((end - start) / 2);
+        int mid = 0;
+        int firstOccurance = -1 ;
 
-            if (a[mid] == target){
-                return mid ;
-            }
-            if (target < a[mid]){
-                end = mid - 1 ;
-            }else {
-                start = mid + 1 ;
-            }
-        }
-        return -1 ;
-    }
-    public static int binarySearchForDescendingOrder(int[] a, int target){
-        int start = 0 ;
-        int end = a.length - 1 ;
-        int mid = 0 ;
-        while (start <= end){
+        while (start <= end) {
             mid = start + ((end - start) / 2);
             if (a[mid] == target){
-                return mid ;
+                firstOccurance = mid ;
+                end = mid - 1 ;
+
             }
-            if (a[mid] < target) {
+            else if(a[mid] > target){
+                end = mid - 1 ;
+            }else{
+                start = mid + 1 ;
+            }
+        }
+
+        return firstOccurance ;
+    }
+    public static int lastOccurance(int []a, int target){
+        int start = 0 ;
+        int end = a.length - 1;
+        int lastOccurance = -1 ;
+        while (start <= end){
+            int mid = start + ((end - start) / 2) ;
+            if (a[mid] == target){
+                lastOccurance = mid ;
+                start = mid + 1 ;
+            }else if (a[mid] > target){
                 end = mid - 1 ;
             }else {
                 start = mid + 1 ;
             }
         }
-        return -1 ;
+        return lastOccurance ;
+    }
+    public static int[] firstAndLastOccurance(int fs,int ls){
+        List<Integer> list = new ArrayList<>() ;
+        list.add(fs) ;
+        list.add(ls) ;
+
+        return list.stream().mapToInt(i -> i).toArray() ;
     }
 }

bfc5918ca0a900a69df10c80c825bf5ec174e688
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-15 11:44:37 +0530

    Searching in a Nearly Sorted Array | Applied Modified Binary Search

--- Main.java
+++ Main.java
@@ -1,35 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 
     public static void main(String[] args) {
-
-        // Find Minimum in Rotated Sorted Array
-        int[] a = { 11, 12,15,18,2,5,6,8};
-        int n = a.length ;
-        int ans = findMin(a, n) ;
+        // Searching in a Nearly Sorted Array
+        int a[] = {5,10,30,20,40} ;
+        int ans = modifiedBinarySearch(a,30) ;
         System.out.println(ans);
     }
 
-    public static int findMin(int[] arr, int n) {
-        int start = 0, end = n - 1;
-        while (start <= end) {
+    public static int modifiedBinarySearch(int[] a, int x){
+        int start = 0 ;
+        int end = a.length - 1 ;
 
-            // if first element is mid or
-            // last element is mid
-            // then simply use modulo so it
-            // never goes out of bound.
-            int mid = start + (end - start) / 2;
-            int prev = (mid - 1 + n) % n;
-            int next = (mid + 1) % n;
-
-            if (arr[mid] <= arr[prev]
-                    && arr[mid] <= arr[next])
-                return mid;
-            else if (arr[mid] <= arr[end])
-                end = mid - 1;
-            else if (arr[mid] >= arr[start])
-                start = mid + 1;
+        while (start <= end){
+            int mid = start + (end - start) / 2 ;
+            if (x == a[mid]){
+                return mid ;
+            }
+            if (mid - 1 >= start && a[mid - 1] == x){
+                return mid - 1;
+            }
+            if (mid + 1 <= end && a[mid + 1 ] == x){
+                return  mid + 1 ;
+            }
+            else if(a[mid] <= x){
+                start = mid + 2 ;
+            }else {
+                end = mid - 2 ;
+            }
         }
-        return 0;
+        return -1 ;
     }
 }

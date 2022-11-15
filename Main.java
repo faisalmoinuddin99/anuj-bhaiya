9ba1eb190627b036f085f8a849460fa56bb5b36d
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-15 10:02:14 +0530

    Find the Rotation Count in Rotated Sorted array

--- Main.java
+++ Main.java
@@ -1,40 +1,35 @@
-import java.util.*;
-
 public class Main {
 
 
     public static void main(String[] args) {
 
         // Find Minimum in Rotated Sorted Array
-        int[] a = {4,5,6,7,0,1,2};
-        int ans = findMin(a, 0) ;
+        int[] a = { 11, 12,15,18,2,5,6,8};
+        int n = a.length ;
+        int ans = findMin(a, n) ;
         System.out.println(ans);
     }
 
-    public static int findMin(int[] a, int target) {
-        int start = 0 ;
-        int end = a.length - 1;
+    public static int findMin(int[] arr, int n) {
+        int start = 0, end = n - 1;
         while (start <= end) {
-            int mid = (start + end) >> 1 ;
-            if (a[mid] == target){
-                return mid ;
-            }
-            // left part is sorted
-            if (a[start] <= a[mid]){
-                // now check the position of target
-                if(target >= a[start] && target <= a[mid]){
-                    end = mid - 1 ;
-                }else {
-                    start = mid + 1 ;
-                }
-            }else {
-                if (target >= a[mid] && target <= a[end]){
-                    start = mid + 1 ;
-                }else  {
-                    end = mid - 1 ;
-                }
-            }
+
+            // if first element is mid or
+            // last element is mid
+            // then simply use modulo so it
+            // never goes out of bound.
+            int mid = start + (end - start) / 2;
+            int prev = (mid - 1 + n) % n;
+            int next = (mid + 1) % n;
+
+            if (arr[mid] <= arr[prev]
+                    && arr[mid] <= arr[next])
+                return mid;
+            else if (arr[mid] <= arr[end])
+                end = mid - 1;
+            else if (arr[mid] >= arr[start])
+                start = mid + 1;
         }
-        return  -1 ;
+        return 0;
     }
 }

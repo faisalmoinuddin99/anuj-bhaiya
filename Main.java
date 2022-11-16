aeadfa23fa343ee27e4b729f8474bbe10130048b
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-16 11:32:59 +0530

    Ceil of an element in a sorted array

--- Main.java
+++ Main.java
@@ -7,8 +7,9 @@ public class Main {
     public static void main(String[] args) {
         // find the floor of an element in a sorted array
         int a[] = {1,2,3,4,8,10,10,12} ;
-        int ans = floorOfAnElementInASortedArray(a,12) ;
-        System.out.println(ans);
+        int ans1 = floorOfAnElementInASortedArray(a,5) ;
+        int ans2 = ceilOfAnElementInASortedArray(a,5) ;
+        System.out.println("Floor: " + ans1 + " Ceil: " + ans2);
     }
 
     public static int floorOfAnElementInASortedArray(int [] a, int target){
@@ -30,5 +31,28 @@ public class Main {
         }
         return resFloor ;
     }
+    public static int ceilOfAnElementInASortedArray(int [] a, int target){
+        int start = 0 ;
+        int end = a.length - 1 ;
 
+        int ceilRes = -1 ;
+        while (start <= end){
+            int mid = start + (end - start) / 2 ;
+
+            if (a[mid] == target){
+                return mid ;
+            }
+            else if(a[mid] > target){
+                ceilRes = a[mid] ;
+                end = mid - 1 ;
+            }else {
+                start = mid + 1 ;
+            }
+        }
+        return ceilRes ;
+    }
 }
+
+/*
+Floor: 4 Ceil: 8
+ */

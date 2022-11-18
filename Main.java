97805d274111f0294b5f6dea00f8ed5735a5c13c
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-18 15:37:56 +0530

    Find the element in infinite Sorted Array

--- Main.java
+++ Main.java
@@ -4,30 +4,38 @@ import static java.util.Arrays.stream;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-            Find position of set bit
-         */
-        int ans = findPosition(1) ;
-        System.out.println(ans);
-    }
+     
+     int a[] = {1,2,3,4,5,6,7,8,9,10,12,14,34,78} ;
 
-   public static int findPosition(int N) {
-        // code here
-       int cnt = 0 ;
-       if (!isPowerOf2(N)){
-           return -1 ;
-       }else{
-           while(N != 0){
-               if((N & 1) == 1){
-                   break ;
-               }
-               cnt++ ;
-               N = N >> 1 ;
-           }
-       }
-       return cnt + 1;
+    int res = findTheElementInInfiniteSortedArray(a,9) ;
+    System.out.println(res) ;
+
     }
-    public static boolean isPowerOf2(int n){
-        return n > 0 && (n & (n-1)) == 0 ;
-    }
+    public static int findTheElementInInfiniteSortedArray(int []a, int key){
+        int start = 0 ;
+        int end = 1 ;
+
+        while(key > a[end]){
+            start = end ;
+            end = end * 2 ;
+        }
+       int ans = binarySearchImpl(a,start,end, key) ;
+
+       return ans ;
+    }   
+
+    public static int binarySearchImpl(int[] a, int start, int end ,int key){
+        while(start <= end){
+            int mid = start + (end - start) / 2 ;
+            if(a[mid] == key){
+                return mid ;
+            }
+            else if(a[mid] > key){
+                end = mid - 1 ;
+            }else {
+                start = mid + 1 ;
+            }
+        }
+        return -1 ;
+    } 
 }

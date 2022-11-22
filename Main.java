cff333a8cbe750e81f5a3f2879561412fbd83b23
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-22 10:51:02 +0530

    Nearest Smaller to Left a.k.a Nearest Smaller element

--- Main.java
+++ Main.java
@@ -7,37 +7,34 @@ public class Main {
 
     public static void main(String[] args) {
         // Next largest element
-        int a[] = {1,3,2,4} ;
-        List<Integer> ll = nextLargestElementToLeft(a,a.length) ;
+        int a[] = {4,5,2,10,8} ;
+        List<Integer> ll = nearestSmallerElementToLeft(a,a.length) ;
         for (int i : ll){
-            System.out.print(i + " "); // -1 -1 3 -1 correct
+            System.out.print(i + " "); // -1 4 -1 2 2 correct
         }
     }
 
-    public static List<Integer> nextLargestElementToLeft(int[]a, int n){
+    public static List<Integer> nearestSmallerElementToLeft(int[]a, int n){
 
         List<Integer> vector = new ArrayList<>() ;
         Stack<Integer> s = new Stack<>() ;
 
-        for (int i = 0 ; i < n; i++){
+        for(int i = 0 ; i < n; i++){
             if (s.empty()){
                 vector.add(-1) ;
             }
-            else if (!s.empty() && a[i] < s.peek()){
-                // idhar hum aainge
+            else if (!s.empty() && a[i] > s.peek()){
                 vector.add(s.peek()) ;
             }
-            else if (!s.empty() && a[i] > s.peek()){
-                while (!s.empty() && a[i] >= s.peek()){
-                    s.pop() ;
-                }
-                if (s.empty()){
-                    vector.add(-1) ;
-                }
-                else {
-                    // hum aainge
-                    vector.add(s.peek()) ;
-                }
+           else if (!s.empty() && a[i] < s.peek()){
+               while (!s.empty() && s.peek() >= a[i]){
+                   s.pop() ;
+               }
+               if (s.empty()){
+                   vector.add(-1) ;
+               }else {
+                   vector.add(s.peek()) ;
+               }
             }
             s.push(a[i]) ;
         }

b6ad38eb8c280b888d70db773c29ac52f9fa15be
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-22 09:46:43 +0530

    Nearest Greater to Left

--- Main.java
+++ Main.java
@@ -7,38 +7,42 @@ public class Main {
 
     public static void main(String[] args) {
         // Next largest element
-        int a[] = {1,3,4,2} ;
-        List<Integer> ll = nextLargestElement(a,a.length) ;
+        int a[] = {1,3,2,4} ;
+        List<Integer> ll = nextLargestElementToLeft(a,a.length) ;
         for (int i : ll){
-            System.out.print(i + " ");
+            System.out.print(i + " "); // -1 -1 3 -1 correct
         }
     }
 
-    public static List<Integer> nextLargestElement(int[]a, int n){
+    public static List<Integer> nextLargestElementToLeft(int[]a, int n){
 
-        List<Integer> list = new ArrayList<>() ;
+        List<Integer> vector = new ArrayList<>() ;
         Stack<Integer> s = new Stack<>() ;
 
-       for (int i = n - 1; i >= 0; i--){
-           if (s.empty()){
-               list.add(-1) ;
-           }
-            else if(!s.empty() && a[i] < s.peek()){
-                list.add(s.peek()) ;
-           }
-            else if(!s.empty() && a[i] > s.peek()){
-                while (s.size() > 0 && s.peek() <= a[i]){
+        for (int i = 0 ; i < n; i++){
+            if (s.empty()){
+                vector.add(-1) ;
+            }
+            else if (!s.empty() && a[i] < s.peek()){
+                // idhar hum aainge
+                vector.add(s.peek()) ;
+            }
+            else if (!s.empty() && a[i] > s.peek()){
+                while (!s.empty() && a[i] >= s.peek()){
                     s.pop() ;
                 }
-                if (s.isEmpty()){
-                    list.add(-1) ;
-                }else {
-                    list.add(s.peek()) ;
+                if (s.empty()){
+                    vector.add(-1) ;
                 }
-           }
-           s.push(a[i]) ;
-       }
-        return list ;
+                else {
+                    // hum aainge
+                    vector.add(s.peek()) ;
+                }
+            }
+            s.push(a[i]) ;
+        }
+
+        return vector;
     }
 }
 

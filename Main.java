863a356eaf04bb0886a1180ee058d58e0f9b1f62
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-23 11:23:25 +0530

    Stock Span Problem | Approach 1

--- Main.java
+++ Main.java
@@ -1,46 +1,38 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
-
-
     public static void main(String[] args) {
-        // Next largest element
-        int a[] = {4,5,2,10,8} ;
-        List<Integer> ll = nearestSmallerElementToRight(a,a.length) ;
-        for (int i : ll){
-            System.out.print(i + " "); // -1 8 -1 2 2 correct
+
+        //  Stock Span Problem
+        int[] a = {100,80,60,70,60,75,85} ;
+
+        List<Integer> ans = stockSpanProblem(a,a.length) ;
+        for (int i : ans){
+            System.out.print(i + " ");
         }
     }
 
-    public static List<Integer> nearestSmallerElementToRight(int[]a, int n){
-
+    public static List<Integer> stockSpanProblem(int[] a, int n) {
+        Stack<Integer> stack = new Stack<>() ;
         List<Integer> vector = new ArrayList<>() ;
-        Stack<Integer> s = new Stack<>() ;
-
-        for (int i = n - 1; i >= 0; i--){
-            if (s.empty()){
-                vector.add(-1) ;
+        for (int i = 0 ; i < n; i++){
+            int cnt = 1 ;
+            if (stack.empty()){
+                vector.add(1) ;
             }
-            else if (!s.empty() && a[i] > s.peek()){
-               vector.add(s.peek()) ;
-            }
-            else if (!s.empty() && a[i] < s.peek()){
-                while (!s.empty() && a[i] <= s.peek()){
-                    s.pop() ;
+            else if (!stack.empty() && a[i] <= stack.peek()){
+                vector.add(1) ;
+            }else if (!stack.empty() && a[i] >= stack.peek()){
+                for (int j = i-1 ; j >= 0; j--) {
+                    if (a[i] > stack.get(j)) {
+                        cnt++ ;
+                    }else break;
                 }
-                if (s.empty()){
-                    vector.add(-1) ;
-                }else{
-                    vector.add(s.peek()) ;
-                }
+                vector.add(cnt) ;
             }
-            s.push(a[i]) ;
+            stack.push(a[i]) ;
         }
-
-
-        return vector;
+       return vector ;
     }
 }
 

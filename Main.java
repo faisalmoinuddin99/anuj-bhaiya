a54afe0aae6a4d293e144b88e1595b352622b7d9
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-23 12:19:49 +0530

    Stock Span Problem | NGL approach

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
 import java.util.*;
 
 public class Main {
+
+    static class Pair{
+        int first ;
+        int second ;
+
+        public Pair(int key, int value){
+            this.first = key ;
+            this.second = value ;
+        }
+
+    }
     public static void main(String[] args) {
 
         //  Stock Span Problem
@@ -8,31 +19,46 @@ public class Main {
 
         List<Integer> ans = stockSpanProblem(a,a.length) ;
         for (int i : ans){
-            System.out.print(i + " ");
+            System.out.print(i + " "); // 1 1 1 2 1 4 6  ANS
         }
+    }
+    public static List<Integer> nearestGreatestElementToLeft(int[] a, int n){
+
+        List<Integer> vector = new ArrayList<>() ;
+        Stack<Pair> stack = new Stack<>() ;
+        for (int i = 0 ; i < n; i++){
+            if (stack.empty()){
+                vector.add(-1) ;
+            }
+            else if (!stack.empty() && a[i] < stack.peek().first){
+                vector.add(stack.peek().second) ;
+            }
+            else if(!stack.empty() && a[i] > stack.peek().first){
+                while (!stack.empty() && a[i] >= stack.peek().first){
+
+
+                    stack.pop() ;
+                }
+                if (stack.empty()){
+                    vector.add(-1) ;
+                }else {
+                    vector.add(stack.peek().second) ;
+                }
+            }
+            stack.push(new Pair(a[i],i)) ;
+        }
+        return vector ;
     }
 
-    public static List<Integer> stockSpanProblem(int[] a, int n) {
-        Stack<Integer> stack = new Stack<>() ;
-        List<Integer> vector = new ArrayList<>() ;
-        for (int i = 0 ; i < n; i++){
-            int cnt = 1 ;
-            if (stack.empty()){
-                vector.add(1) ;
-            }
-            else if (!stack.empty() && a[i] <= stack.peek()){
-                vector.add(1) ;
-            }else if (!stack.empty() && a[i] >= stack.peek()){
-                for (int j = i-1 ; j >= 0; j--) {
-                    if (a[i] > stack.get(j)) {
-                        cnt++ ;
-                    }else break;
-                }
-                vector.add(cnt) ;
-            }
-            stack.push(a[i]) ;
+    public static List<Integer> stockSpanProblem(int[] a, int n){
+
+        List<Integer> vector1 = nearestGreatestElementToLeft(a,n) ;
+        List<Integer> finalVector = new ArrayList<>() ;
+        for(int i = 0 ; i < n; i++){
+            finalVector.add(i - vector1.get(i)) ;
         }
-       return vector ;
+        return finalVector ;
     }
+
 }
 

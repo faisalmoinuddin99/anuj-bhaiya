521b22d16f1c3c15950258aa80c1305f5c5e1f6a
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-11-28 17:30:55 +0530

    Max Area of Histogram

--- src/Main.java
+++ src/Main.java
@@ -2,113 +2,106 @@ import java.util.*;
 
 public class Main {
 
-    static public class Pair {
-        private int first;
-        private int second;
+    static public class Pair{
+        private int first ;
+        private int second ;
 
-        public Pair(int key, int value) {
-            this.first = key;
-            this.second = value;
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
         }
     }
-
     public static void main(String[] args) {
-        // Maximum area of histogram
-        int[] a = {5,5,1,7,1,1,5,2,7,6};
+        int[] a = {5,5,1,7,1,1,5,2,7,6} ;
+        int n = a.length ;
 
-        int leet = largestRectangleArea(a) ;
-        System.out.println("Max area of histogram: "+ leet);
-    }
-    public static int largestRectangleArea(int[] heights) {
-        List<Integer> ans = maxAreaOfHistogram(heights);
-        for (int i : ans){
-            System.out.print(i + " ");
-        }
-        return Collections.max(ans) ;
-    }
-
-    public static List<Integer> maxAreaOfHistogram(int []a){
-        List<Integer> NSLIndex = NSL(a) ; // -1 -1 1 1 3 -1 5
-        List<Integer> NSRIndex = NSR(a) ;
-        Collections.reverse(NSRIndex); // 1 5 3 5 5 7 7
-
-
-        // print NSR and NSL
-        System.out.println("Nearest Smallest to Right");
-        for (int i : NSRIndex) {
-            System.out.print(i + " ");
-        }
+      int maxLengthOfHistogram =  getMaxArea(a,n) ;
         System.out.println();
-        System.out.println("Nearest Smallest to Left");
-        for (int i : NSLIndex) {
+        System.out.println("Max height: " + maxLengthOfHistogram);
+    }
+    public static int getMaxArea(int[] arr, int size){
+        int width[] = new int[size] ;
+        int right[] = new int[size] ;
+        int left [] = new int[size] ;
+        int height[] = new int[size] ;
+        right = NSR(arr,size) ;
+        left = NSL(arr,size) ;
+        for (int i = 0; i < size; i++){
+            width[i] = right[i] - left[i] - 1 ;
+            height[i] = arr[i] * width[i] ;
+        }
+        System.out.println("width: ");
+        for (int i: width){
             System.out.print(i + " ");
         }
-        System.out.println();
-
-
-
-
-        // width[i] = NSRIndex[i] - NSLIndex[i] - 1
-
-        List<Integer> width = new ArrayList<>() ;
-        for (int i = 0 ; i < NSLIndex.size(); i++){
-            int widthOfRectangle = NSRIndex.get(i) - NSLIndex.get(i) ;
-            width.add(widthOfRectangle) ;
+        System.out.println("Height: ");
+        for (int i: height){
+            System.out.print(i + " ");
         }
-        List<Integer> heights = new ArrayList<>() ;
-        for (int i = 0 ; i < width.size(); i++){
-           // calculate area of histogram
-            heights.add(width.get(i) * a[i]) ;
-        }
-        return heights ;
+
+        return Arrays.stream(height).max().getAsInt() ;
     }
-
-    public static List<Integer> NSR(int[] a) {
-        List<Integer> rightList = new ArrayList<>();
-        Stack<Pair> stack = new Stack<>();
-
-        for (int i = a.length - 1; i >= 0; i--) {
-            if (stack.empty()) {
-                rightList.add(a.length);
-            } else if (!stack.empty() && a[i] >= stack.peek().first) {
-                rightList.add(stack.peek().second);
+    public static int[] NSR(int[] a ,int size){
+        Stack<Pair> stack = new Stack<>() ;
+        int[] right = new int[size] ;
+        for (int i = size - 1; i >= 0 ; i--){
+            if (stack.empty()){
+                right[i] = size ;
+            } else if (!stack.empty() && a[i] > stack.peek().first) {
+                right[i] = stack.peek().second ;
             } else if (!stack.empty() && a[i] <= stack.peek().first) {
-                while (!stack.empty() && a[i] <= stack.peek().first) {
-                    stack.pop();
+                while (!stack.empty() && a[i] <= stack.peek().first){
+                    stack.pop() ;
                 }
-                if (stack.empty()) {
-                    rightList.add(a.length);
-                } else {
-                    rightList.add(stack.peek().second);
+                if (stack.empty()){
+                    right[i] = size ;
+                }else {
+                    right[i] = stack.peek().second ;
                 }
             }
-            stack.push(new Pair(a[i], i));
+            stack.push(new Pair(a[i],i)) ;
         }
-        return rightList;
+        System.out.print("Smallest element to Right: ");
+        for (int i : right){
+            System.out.print(i + " ");
+        }
+        return right;
     }
-
-    public static List<Integer> NSL(int[] a) {
-        List<Integer> leftList = new ArrayList<>();
-        Stack<Pair> stack = new Stack<>();
-        int pseudoIndex = -1 ;
-        for (int i = 0; i < a.length; i++) {
-            if (stack.empty()) {
-                leftList.add(pseudoIndex);
-            } else if (!stack.empty() && a[i] >= stack.peek().first) {
-                leftList.add(stack.peek().second);
-            } else if (!stack.empty() && a[i] <= stack.peek().first) {
-                while (!stack.empty() && a[i] <= stack.peek().first) {
-                    stack.pop();
+    public static int[] NSL(int[] a, int n){
+        Stack<Pair> stack = new Stack<>() ;
+        int[] left = new int[n] ;
+        for (int i = 0 ; i < n; i++){
+            if (stack.empty()){
+                left[i] = -1 ;
+            }
+            else if(!stack.empty() && a[i] > stack.peek().first){
+                    left[i] = stack.peek().second ;
+            }
+            else if (!stack.empty() && a[i] <= stack.peek().first){
+                while (!stack.empty() && a[i] <= stack.peek().first){
+                    stack.pop() ;
                 }
-                if (stack.empty()) {
-                    leftList.add(pseudoIndex);
-                } else {
-                    leftList.add(stack.peek().second);
+                if (stack.empty()){
+                    left[i] = -1 ;
+                }else {
+                    left[i] = stack.peek().second ;
                 }
             }
-            stack.push(new Pair(a[i], i));
+            stack.push(new Pair(a[i], i)) ;
         }
-
-        return leftList;
+        System.out.print("Smallest element to Left: ");
+        for (int i : left){
+            System.out.print(i + " ");
+        }
+        return left ;
     }
 }
+
+/*
+a = {5,5,1,7,1,1,5,2,7,6} ;
+Smallest element to Right Index: 2 2 10 4 10 10 7 10 9 10  here -1 is replaced with = a.length
+The Smallest element to Left Index: -1 -1 -1 2 -1 -1 5 5 7 7
+width: 2 2 10 1 10 10 1 4 1 2
+Height: 10 10 10 7 10 10 5 8 7 12
+Max height: 12
+ */
